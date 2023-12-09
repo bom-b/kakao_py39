24f9a5e282b8184930c347c56c364eb0e3a6af32
heyou <sb97213@gamil.com>
2023-12-09 23:52:27 +0900

    텐서

--- kakao_py39/settings.py
+++ kakao_py39/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os, environ
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,9 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = environ.Env()
-environ.Env.read_env()
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "django-insecure-9)g+eq^!z1)=vd*dm$@3#c8vx=(8^je%@d3rmj)8yfd8lxp3*c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
