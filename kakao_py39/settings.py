3fecd5d4d82f1f180e4aa875ce7626be0a98c4cc
heyou <sb97213@gamil.com>
2023-12-10 19:18:34 +0900

    접속 테스트

--- kakao_py39/settings.py
+++ kakao_py39/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import json
 import os
 from pathlib import Path
+
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +22,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9)g+eq^!z1)=vd*dm$@3#c8vx=(8^je%@d3rmj)8yfd8lxp3*c"
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*', '116.255.102.141']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
--- main/views.py
+++ main/views.py
@@ -15,10 +15,13 @@ logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def communication_test(request):
-    load_json = json.loads(request.body.decode('utf8'))
-    # print(str(load_json))
-    plusfriend_user_key = load_json.get('userRequest', {}).get('user', {}).get('properties', {}).get(
-        'plusfriend_user_key', None)
+    try:
+        load_json = json.loads(request.body.decode('utf8'))
+        # print(str(load_json))
+        plusfriend_user_key = load_json.get('userRequest', {}).get('user', {}).get('properties', {}).get(
+            'plusfriend_user_key', '접속 성공 했으나 유저 정보 없음')
+    except:
+        plusfriend_user_key = '정상 접속이 아님'
 
     return JsonResponse({
         "version": "2.0",
