438684afcf3b8c5c3b8dc1cc6cf788d1c43a9cf3
heyou <sb97213@gamil.com>
2023-12-11 09:23:54 +0900

    멤버 로그인

--- main/views.py
+++ main/views.py
@@ -17,12 +17,12 @@ logger = logging.getLogger(__name__)
 def communication_test(request):
     try:
         load_json = json.loads(request.body.decode('utf8'))
-        # print(str(load_json))
+        print(str(load_json))
         plusfriend_user_key = load_json.get('userRequest', {}).get('user', {}).get('properties', {}).get(
             'plusfriend_user_key', '접속 성공 했으나 유저 정보 없음')
     except:
         plusfriend_user_key = '정상 접속이 아님'
-
+    # print(plusfriend_user_key)
     return JsonResponse({
         "version": "2.0",
         "templates": {
--- member/views.py
+++ member/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 # Create your views here.
 def login_page(request):
     return render(request, "member/login.html")
+
+
